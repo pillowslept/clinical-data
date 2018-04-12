587cc084661e0ced5bb8d377164c3c917fd0afd5
Juan Camilo Velasquez <juancamilo---142@hotmail.com>
2018-04-11 21:45:18 -0500

    Se adiciona parámetro de entrada a objeto.

--- src/main/java/co/edu/itm/clinicaldata/dto/Params.java
+++ src/main/java/co/edu/itm/clinicaldata/dto/Params.java
@@ -2,9 +2,16 @@ package co.edu.itm.clinicaldata.dto;
 
 public class Params {
 
-	private String userName;
+	private String language;
 	private String function;
+	private String userName;
 
+	public String getLanguage() {
+		return language;
+	}
+	public void setLanguage(String language) {
+		this.language = language;
+	}
 	public String getUserName() {
 		return userName;
 	}
