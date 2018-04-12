938a02d234acf2262a66060983c77f20a2a54304
Juan Camilo Velasquez <juancamilo---142@hotmail.com>
2018-04-11 22:10:34 -0500

    Se crea método que valida si un id de tipo long es válido.

--- src/main/java/co/edu/itm/clinicaldata/util/Validations.java
+++ src/main/java/co/edu/itm/clinicaldata/util/Validations.java
@@ -5,4 +5,8 @@ public class Validations {
 	public static boolean field(String field){
 		return field == null || field.isEmpty();
 	}
+
+	public static boolean field(Long field){
+        return field == null || field <= 0;
+    }
 }
