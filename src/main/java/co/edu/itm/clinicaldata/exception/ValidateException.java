dc221fa6079bac3ceb08edafe30440ae2590aaeb
Juan Camilo Velasquez <juancamilo---142@hotmail.com>
2018-04-11 21:46:42 -0500

    Se crea exception personalizada para validaciones.

---
+++ src/main/java/co/edu/itm/clinicaldata/exception/ValidateException.java
@@ -0,0 +1,20 @@
+package co.edu.itm.clinicaldata.exception;
+
+public class ValidateException extends Exception {
+
+	public ValidateException() {
+		super();
+	}
+
+	public ValidateException(String message) {
+		super(message);
+	}
+
+	public ValidateException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public ValidateException(Throwable cause) {
+		super(cause);
+	}
+}
