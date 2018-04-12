6d6eaa3b7626cc55c2ed94f3ff84c30daa874af3
Juan Camilo Velasquez <juancamilo---142@hotmail.com>
2018-04-11 21:47:27 -0500

    Se crea mapper de exceptions a lanzar en la aplicación.

---
+++ src/main/java/co/edu/itm/clinicaldata/mapper/ApiError.java
@@ -0,0 +1,50 @@
+package co.edu.itm.clinicaldata.mapper;
+
+import org.springframework.http.HttpStatus;
+
+public class ApiError {
+
+	private HttpStatus status;
+	private int statusCode;
+	private String message;
+	private String debugMessage;
+
+	ApiError(HttpStatus status, Throwable ex) {
+		this.status = status;
+		this.statusCode = status.value();
+		this.message = ex.getMessage();
+		this.debugMessage = ex.getLocalizedMessage();
+	}
+
+	public HttpStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(HttpStatus status) {
+		this.status = status;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getDebugMessage() {
+		return debugMessage;
+	}
+
+	public void setDebugMessage(String debugMessage) {
+		this.debugMessage = debugMessage;
+	}
+
+	public int getStatusCode() {
+		return statusCode;
+	}
+
+	public void setStatusCode(int statusCode) {
+		this.statusCode = statusCode;
+	}
+}
---
+++ src/main/java/co/edu/itm/clinicaldata/mapper/Mapper.java
@@ -0,0 +1,24 @@
+package co.edu.itm.clinicaldata.mapper;
+
+import org.apache.log4j.Logger;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import co.edu.itm.clinicaldata.exception.ValidateException;
+import co.edu.itm.clinicaldata.service.ProcessDataService;
+
+@ControllerAdvice
+public class Mapper extends ResponseEntityExceptionHandler {
+
+	private static final Logger LOGGER = Logger.getLogger(ProcessDataService.class.getName());
+
+	@ExceptionHandler(ValidateException.class)
+	protected ResponseEntity<Object> handleEntityNotFound(ValidateException ex) {
+		ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST, ex);
+		LOGGER.error(ex.getMessage(), ex);
+		return new ResponseEntity<>(apiError, apiError.getStatus());
+	}
+}
