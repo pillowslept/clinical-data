cbcdac071129c0510ecb3f001028e5e5174a4a4c
Juan Camilo Velasquez <juancamilo---142@hotmail.com>
2018-04-15 22:43:41 -0500

    Se adicionan métodos para creación de archivos con nombre único.

--- src/main/java/co/edu/itm/clinicaldata/util/FileUtilities.java
+++ src/main/java/co/edu/itm/clinicaldata/util/FileUtilities.java
@@ -4,10 +4,12 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.UUID;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.log4j.Logger;
 
-import co.edu.itm.clinicaldata.model.Languages;
+import co.edu.itm.clinicaldata.model.Language;
 
 public class FileUtilities {
 
@@ -16,29 +18,40 @@ public class FileUtilities {
     private static final String FILE_SEPARATOR = System.getProperty("file.separator");
     private static final Logger LOGGER = Logger.getLogger(FileUtilities.class.getName());
 
-    public static void createFile(String functionToProcess, String fileName, Languages language) {
+    public static void createFile(String functionToProcess, String path) {
         try {
-            Files.write(Paths.get(buildBasePath(language) + fileName), functionToProcess.getBytes());
+            Files.write(Paths.get(path), functionToProcess.getBytes());
         } catch (IOException ex) {
             LOGGER.error("Ocurrió un error generando el archivo con la función", ex);
         }
     }
-    
-    public static String buildBasePath(Languages language){
-        String basePath = USER_HOME + FILE_SEPARATOR + FOLDER_NAME + FILE_SEPARATOR + language.toString() + FILE_SEPARATOR;
+
+    public static String buildBasePath(String languageFolder){
+        String basePath = USER_HOME + FILE_SEPARATOR + FOLDER_NAME + FILE_SEPARATOR + languageFolder + FILE_SEPARATOR;
         createBasePath(basePath);
         return basePath;
     }
-    
+
     public static void createBasePath(String basePath){
         new File(basePath).mkdirs();
+    }
+
+    public static String randomIdentifier(){
+        return UUID.randomUUID().toString();
+    }
+
+    public static String generateFileName(String randomIdentifier, String ext){
+        return String.format("%s.%s", randomIdentifier, ext);
     }
 
     public static void main(String[] args) {
         // Aquí las instrucciones del método
         System.getProperties().list(System.out);
-        createFile("Esta es la función que quiero ejecutar y ensayar", "Java_123456.java", Languages.JAVA);
-        createFile("Esta es la función que quiero ejecutar y ensayar", "Python_123456.py", Languages.PYTHON);
-        createFile("Esta es la función que quiero ejecutar y ensayar", "Python_123456.r", Languages.R);
+        createFile("Esta es la función que quiero ejecutar y ensayar", buildBasePath(Language.JAVA.getName()) + generateFileName(randomIdentifier(), "java"));
+        createFile("Esta es la función que quiero ejecutar y ensayar", buildBasePath(Language.PYTHON.getName()) + generateFileName(randomIdentifier(), "py"));
+        createFile("Esta es la función que quiero ejecutar y ensayar", buildBasePath(Language.R.getName()) + generateFileName(randomIdentifier(), "r"));
+        System.out.println(String.format("%s.%s", RandomStringUtils.randomAlphanumeric(16), "java"));
+        System.out.println(generateFileName(randomIdentifier(), "java"));
     }
+
 }
