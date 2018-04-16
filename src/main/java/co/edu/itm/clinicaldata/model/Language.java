be86e7d58a5aca238bd1911692691decef4c0c3a
Juan Camilo Velasquez <juancamilo---142@hotmail.com>
2018-04-15 22:43:19 -0500

    Se modifica enum de lenguaje.

--- src/main/java/co/edu/itm/clinicaldata/model/Language.java
+++ src/main/java/co/edu/itm/clinicaldata/model/Language.java
@@ -1,9 +1,25 @@
 package co.edu.itm.clinicaldata.model;
 
-public enum Languages {
+public enum Language {
 	
-    JAVA,
-    PYTHON,
-    R;
+    JAVA("java", "java"),
+    PYTHON("python", "py"),
+    R("r", "r");
+
+    private String fileExtension;
+    private String name;
+
+    Language(String name, String fileExtension) {
+        this.name = name;
+        this.fileExtension = fileExtension;
+    }
+
+    public String getFileExtension() {
+        return fileExtension;
+    }
+
+    public String getName() {
+        return name;
+    }
 
 }
