fca13c6d22d6123fc1782a715a11060129dfa187
Juan Camilo Velasquez <juancamilo---142@hotmail.com>
2018-04-30 13:30:27 -0500

    Se expone servicio para subir archivos planos. (Java, Python, R)

---
+++ src/main/java/co/edu/itm/clinicaldata/controller/UploadFileController.java
@@ -0,0 +1,29 @@
+package co.edu.itm.clinicaldata.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import co.edu.itm.clinicaldata.exception.ValidateException;
+import co.edu.itm.clinicaldata.service.FileService;
+
+@Controller
+@RequestMapping("/api/file")
+public class UploadFileController {
+
+    @Autowired
+    FileService fileService;
+
+    @RequestMapping(value = "/upload", method = RequestMethod.POST)
+    public ResponseEntity<String> upload(
+            @RequestParam("file") MultipartFile file) throws ValidateException {
+        return new ResponseEntity<String>(fileService.upload(file),
+                HttpStatus.OK);
+    }
+
+}
---
+++ src/main/java/co/edu/itm/clinicaldata/service/FileService.java
@@ -0,0 +1,95 @@
+package co.edu.itm.clinicaldata.service;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.commons.io.FilenameUtils;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import co.edu.itm.clinicaldata.enums.Language;
+import co.edu.itm.clinicaldata.exception.ValidateException;
+import co.edu.itm.clinicaldata.model.ProcessingRequest;
+import co.edu.itm.clinicaldata.util.FileUtilities;
+
+@Service
+public class FileService {
+
+    private static final Logger LOGGER = Logger.getLogger(FileService.class
+            .getName());
+
+    @Autowired
+    ProcessingRequestService processingRequestService;
+
+    private final List<String> languagesAllowed = Arrays.asList(
+            Language.JAVA.toString(), Language.PYTHON.toString(),
+            Language.R.toString());
+
+    public String upload(MultipartFile file) throws ValidateException {
+        String fileExtension = FilenameUtils.getExtension(file
+                .getOriginalFilename());
+        validateExtensionAllowed(fileExtension);
+
+        Language language = getLanguage(fileExtension);
+        byte[] bytes = getBytesFromFile(file);
+        String fileName = file.getOriginalFilename();
+        String basePath = FileUtilities.buildBasePath(language.getName());
+
+        FileUtilities.createFile(bytes,
+                buildPath(basePath, file.getOriginalFilename()));
+
+        ProcessingRequest processingRequest = processingRequestService
+                .createProcessingRequest(language.getName(), bytes, fileName,
+                        basePath);
+        
+        return String
+                .format("El archivo ha sido almacenado con éxito, identificador generado: <%s>.",
+                        processingRequest.getIdentifier());
+    }
+
+    private String buildPath(String basePath, String fileName){
+        return basePath + fileName;
+    }
+
+    private byte[] getBytesFromFile(MultipartFile file)
+            throws ValidateException {
+        byte[] bytes;
+        try {
+            bytes = file.getBytes();
+        } catch (IOException e) {
+            throw new ValidateException(
+                    "Ocurrió un error obteniendo el archivo a procesar");
+        }
+        return bytes;
+    }
+
+    private void validateExtensionAllowed(String extension)
+            throws ValidateException {
+        boolean validLanguage = languagesAllowed.stream().anyMatch(
+                language -> language.equalsIgnoreCase(extension));
+        if (!validLanguage) {
+            throw new ValidateException(
+                    String.format(
+                            "La extensión del archivo a procesar no es soportada por la aplicación. Extensiones permitidas: %s",
+                            languagesAllowed.toString()));
+        }
+    }
+
+    private Language getLanguage(String languageToProcess) {
+        Language language = null;
+        if (languageToProcess.equalsIgnoreCase(Language.JAVA.getName())) {
+            language = Language.JAVA;
+        } else if (languageToProcess
+                .equalsIgnoreCase(Language.PYTHON.getName())) {
+            language = Language.PYTHON;
+        } else if (languageToProcess.equalsIgnoreCase(Language.R.getName())) {
+            language = Language.R;
+        }
+        LOGGER.info("El lenguaje a procesar es " + language);
+        return language;
+    }
+
+}
