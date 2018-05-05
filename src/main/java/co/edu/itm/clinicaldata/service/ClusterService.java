42a4291e6d4a885dbd88c124f22537742d40c31f
Juan Camilo Velasquez <juancamilo---142@hotmail.com>
2018-05-05 12:09:44 -0500

    Se modifica clase de procesamiento a cluster.

--- src/main/java/co/edu/itm/clinicaldata/service/ClusterService.java
+++ src/main/java/co/edu/itm/clinicaldata/service/ClusterService.java
@@ -10,6 +10,7 @@ import co.edu.itm.clinicaldata.dto.Output;
 import co.edu.itm.clinicaldata.enums.ProcessState;
 import co.edu.itm.clinicaldata.model.ProcessingRequest;
 import co.edu.itm.clinicaldata.util.Commands;
+import co.edu.itm.clinicaldata.util.Validations;
 
 @Service
 public class ClusterService {
@@ -21,39 +22,44 @@ public class ClusterService {
 
     @Async
     public void sendProcessToCluster(ProcessingRequest processingRequest) {
-        LOGGER.info("Comenzando el proceso en el cluster, simulando espera de 20 segundos");
         sleep();
-        LOGGER.info("Cumplida la espera, se comienza a procesar la solicitud");
 
         Output compileOutput = Commands.executeJavaCommand(
                 Commands.JAVA_COMPILE_COMMAND,
                 buildFilePath(processingRequest.getBasePath(),
                         processingRequest.getFileName()));
 
-        if(compileOutput.getError() != null && !compileOutput.getError().isEmpty()){
+        String result = "";
+        ProcessState processState = null;
+        if (Validations.field(compileOutput.getError())) {
+            result = compileOutput.getError();
+            processState = ProcessState.FINISHED_WITH_ERRORS;
             LOGGER.info("Clase no compilada, presenta errores");
-            updateProcessingRequest(processingRequest, compileOutput.getError(), ProcessState.FINISHED_WITH_ERRORS);
-        }else{
+        } else {
             LOGGER.info("Clase compilada con éxito");
             Output executeOutput = Commands.executeJavaCommand(
                     Commands.JAVA_EXECUTE_COMMAND,
                     buildFilePathExecute(processingRequest.getBasePath(),
                             processingRequest.getFileName()));
-            if(executeOutput.getError() != null && !executeOutput.getError().isEmpty()){
+            if (Validations.field(executeOutput.getError())) {
+                result = executeOutput.getError();
+                processState = ProcessState.FINISHED_WITH_ERRORS;
                 LOGGER.info("Clase no ejecutada, presenta errores");
-                updateProcessingRequest(processingRequest, executeOutput.getError(), ProcessState.FINISHED_WITH_ERRORS);
-            }else{
+            } else {
+                result = executeOutput.getResult();
+                processState = ProcessState.FINISHED_OK;
                 LOGGER.info("Clase ejecutada con éxito");
-                updateProcessingRequest(processingRequest, executeOutput.getResult(), ProcessState.FINISHED_OK);
             }
         }
+
+        updateProcessingRequest(processingRequest, result, processState);
     }
 
-    private void updateProcessingRequest(ProcessingRequest processingRequest, String output, ProcessState processState){
+    private void updateProcessingRequest(ProcessingRequest processingRequest,
+            String output, ProcessState processState) {
         processingRequest.setResult(output);
         processingRequest.setState(processState.getState());
         processingRequestService.update(processingRequest);
-        LOGGER.info("Solicitud actualizada en BD");
     }
 
     private String buildFilePath(String basePath, String fileName) {
@@ -64,12 +70,14 @@ public class ClusterService {
         return basePath + ";. " + FilenameUtils.getBaseName(fileName);
     }
 
-    private void sleep(){
+    private void sleep() {
+        LOGGER.info("Comenzando el proceso en el cluster, simulando espera de 20 segundos");
         try {
             Thread.sleep(20000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+        LOGGER.info("Cumplida la espera, se comienza a procesar la solicitud");
     }
 
 }
