360d3015e60b514c24350b3a07cec55662a2d954
Juan Camilo Velasquez <juancamilo---142@hotmail.com>
2018-05-05 14:22:26 -0500

    Se expone servicio de reporte por investidor o por solicitud, se ajusta el reporte con logo y columnas.

--- src/main/java/co/edu/itm/clinicaldata/controller/ReportController.java
+++ src/main/java/co/edu/itm/clinicaldata/controller/ReportController.java
@@ -1,6 +1,8 @@
 package co.edu.itm.clinicaldata.controller;
 
 import java.io.ByteArrayInputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
@@ -13,8 +15,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import co.edu.itm.clinicaldata.exception.ValidateException;
+import co.edu.itm.clinicaldata.model.Investigator;
 import co.edu.itm.clinicaldata.model.ProcessingRequest;
-import co.edu.itm.clinicaldata.service.ProcessDataService;
+import co.edu.itm.clinicaldata.service.InvestigatorService;
+import co.edu.itm.clinicaldata.service.ProcessingRequestService;
 import co.edu.itm.clinicaldata.util.FileUtilities;
 import co.edu.itm.clinicaldata.util.GeneratePdfReport;
 
@@ -23,20 +27,46 @@ import co.edu.itm.clinicaldata.util.GeneratePdfReport;
 public class ReportController {
 
     @Autowired
-    ProcessDataService processDataService;
+    InvestigatorService investigatorService;
 
-    @RequestMapping(value = "/result/{identifier}", method = RequestMethod.GET, produces = MediaType.APPLICATION_PDF_VALUE)
-    public ResponseEntity<InputStreamResource> resultPDF(
+    @Autowired
+    ProcessingRequestService processingRequestService;
+
+    @RequestMapping(value = "/request/{identifier}", method = RequestMethod.GET, produces = MediaType.APPLICATION_PDF_VALUE)
+    public ResponseEntity<InputStreamResource> byRequest(
             @PathVariable("identifier") String identifier)
             throws ValidateException {
 
-        ProcessingRequest processingRequest = processDataService.processResultForReport(identifier);
+        ProcessingRequest processingRequest = processingRequestService.validateAndFindByIdentifier(identifier);
+        List<ProcessingRequest> list = new ArrayList<>();
+        list.add(processingRequest);
 
-        ByteArrayInputStream bis = GeneratePdfReport.processResult(processingRequest);
+        ByteArrayInputStream bis = GeneratePdfReport.processRequest(processingRequest.getInvestigator(), list);
 
+        return buildResponse(identifier, bis);
+    }
+
+    @RequestMapping(value = "/investigator/{investigatorId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_PDF_VALUE)
+    public ResponseEntity<InputStreamResource> byInvestigator(
+            @PathVariable("investigatorId") Long investigatorId)
+            throws ValidateException {
+
+        Investigator investigator = investigatorService.validateAndfind(investigatorId);
+        List<ProcessingRequest> listProcessingRequest = processingRequestService.findByInvestigatorId(investigatorId);
+
+        ByteArrayInputStream bis = GeneratePdfReport.processRequest(investigator, listProcessingRequest);
+
+        return buildResponse(investigator.getName(), bis);
+    }
+
+    private ResponseEntity<InputStreamResource> buildResponse(String name,
+            ByteArrayInputStream bis) {
         HttpHeaders headers = new HttpHeaders();
-        headers.add(HttpHeaders.CONTENT_DISPOSITION, 
-                "inline; filename=" + FileUtilities.createFileName(identifier, FileUtilities.PDF_FILE_EXTENSION));
+        headers.add(
+                HttpHeaders.CONTENT_DISPOSITION,
+                "inline; filename="
+                        + FileUtilities.createFileName(name,
+                                FileUtilities.PDF_FILE_EXTENSION));
 
         return ResponseEntity.ok().headers(headers)
                 .contentType(MediaType.APPLICATION_PDF)
--- src/main/java/co/edu/itm/clinicaldata/util/GeneratePdfReport.java
+++ src/main/java/co/edu/itm/clinicaldata/util/GeneratePdfReport.java
@@ -2,18 +2,23 @@ package co.edu.itm.clinicaldata.util;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.util.ArrayList;
+import java.io.IOException;
+import java.net.URL;
 import java.util.List;
 
 import org.apache.log4j.Logger;
 
+import co.edu.itm.clinicaldata.model.Investigator;
 import co.edu.itm.clinicaldata.model.ProcessingRequest;
 
+import com.itextpdf.text.BadElementException;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Element;
 import com.itextpdf.text.Font;
 import com.itextpdf.text.FontFactory;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.Phrase;
 import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
@@ -21,29 +26,28 @@ import com.itextpdf.text.pdf.PdfWriter;
 
 public class GeneratePdfReport {
 
-    private static final Logger LOGGER = Logger.getLogger(GeneratePdfReport.class.getName());
-
+    private static final int TABLE_100_PERCENT = 100;
+    private static final String ITM_LOGO = "http://clusteri.itm.edu.co/wiki/img/tiki/Tiki_WCG.png";
     private static final int FONT_SIZE_BODY = 8;
     private static final int FONT_SIZE_HEAD = 10;
     private static final Font HEAD_FONT = FontFactory.getFont(FontFactory.HELVETICA_BOLD, FONT_SIZE_HEAD);
     private static final Font BODY_FONT = FontFactory.getFont(FontFactory.HELVETICA, FONT_SIZE_BODY);
 
-    public static ByteArrayInputStream processResult(ProcessingRequest processingRequest) {
+    private static final Logger LOGGER = Logger.getLogger(GeneratePdfReport.class.getName());
+
+    public static ByteArrayInputStream processRequest(Investigator investigator, List<ProcessingRequest> listProcessingRequest) {
         Document document = new Document();
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         try {
             PdfPTable table = new PdfPTable(5);
-            table.setWidthPercentage(100);
+            table.setWidthPercentage(TABLE_100_PERCENT);
             table.setWidths(new int[]{1, 4, 2, 2, 1});
 
             table.addCell(headCell("Id"));
-            table.addCell(headCell("Identifier"));
-            table.addCell(headCell("Creation Date"));
-            table.addCell(headCell("Filename"));
-            table.addCell(headCell("State"));
-
-            List<ProcessingRequest> listProcessingRequest = new ArrayList<>();
-            listProcessingRequest.add(processingRequest);
+            table.addCell(headCell("Identificador"));
+            table.addCell(headCell("F. creación"));
+            table.addCell(headCell("Archivo"));
+            table.addCell(headCell("Estado"));
 
             for (ProcessingRequest request : listProcessingRequest) {
                 table.addCell(bodyCell(request.getId().toString()));
@@ -55,15 +59,71 @@ public class GeneratePdfReport {
 
             PdfWriter.getInstance(document, out);
             document.open();
-            document.add(table);
 
+            document.add(createHeader(investigator, "REPORTE DE PROCESAMIENTO DE SOLICITUD"));
+            document.add(table);
             document.close();
         } catch (DocumentException ex) {
             LOGGER.error(String
-                    .format("Ocurrió un error en la generación del PDF, para la solicitud <%s>",
-                            processingRequest.getIdentifier()), ex);
+                    .format("Ocurrió un error en la generación del PDF, para el investigador <%d>",
+                            investigator.getId()), ex);
         }
         return new ByteArrayInputStream(out.toByteArray());
+    }
+
+    private static PdfPCell getImageHeaderCell() {
+        Image image = null;
+        try {
+            image = Image.getInstance(new URL(ITM_LOGO));
+            image.scalePercent(40);
+        } catch (IOException | BadElementException e) {
+            LOGGER.info("Ocurrió un error obteniendo el logo para el PDF.", e);
+        }
+        PdfPCell cell = new PdfPCell(image);
+        alignCellToCenter(cell);
+        return cell;
+    }
+
+    private static void alignCellToCenter(PdfPCell cell) {
+        cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+        cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+        cell.setBorderWidth(1);
+    }
+
+    private static PdfPCell getNameHeaderCell(String title) {
+        PdfPCell cell = new PdfPCell(new Paragraph(title, HEAD_FONT));
+        alignCellToCenter(cell);
+        return cell;
+    }
+
+    private static PdfPCell headerCell(String name){
+        return new PdfPCell(new Paragraph(name, BODY_FONT));
+    }
+
+    private static PdfPCell getDataHeaderCell(Investigator investigator){
+        PdfPTable table = new PdfPTable(1);
+
+        table.addCell(headerCell("F. de generación: " + DateUtilities.getActualDate()));
+        table.addCell(headerCell("Investigador: " + investigator.getName()));
+        table.addCell(headerCell("Email: " + investigator.getEmail()));
+        table.addCell(headerCell(""));
+
+        PdfPCell cell = new PdfPCell(table);
+        cell.setBorderWidth(1);
+
+        return cell;
+    }
+
+    private static PdfPTable createHeader(Investigator investigator, String reportName) {
+        PdfPTable table = new PdfPTable(3);
+        table.setWidthPercentage(TABLE_100_PERCENT);
+
+        table.addCell(getImageHeaderCell());
+        table.addCell(getNameHeaderCell(reportName));
+        table.addCell(getDataHeaderCell(investigator));
+        table.setSpacingAfter(10);
+
+        return table;
     }
 
     private static PdfPCell headCell(String name){
