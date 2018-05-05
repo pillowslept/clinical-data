22d121c38d3d72bf94a54f431a85275db09a54ba
Juan Camilo Velasquez <juancamilo---142@hotmail.com>
2018-05-04 22:15:11 -0500

    Se crea DTO para representar la respuesta de una ejecución.

---
+++ src/main/java/co/edu/itm/clinicaldata/dto/Output.java
@@ -0,0 +1,24 @@
+package co.edu.itm.clinicaldata.dto;
+
+public class Output {
+
+    private String error;
+    private String result;
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+}
