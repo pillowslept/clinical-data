b1d6f33352e196a19c21bd3ab7c42deb8df9d34b
Juan Camilo Velasquez <juancamilo---142@hotmail.com>
2018-05-09 21:47:39 -0500

    Se crea clase de cola de espera para iterar sobre los procesamientos.

---
+++ src/main/java/co/edu/itm/clinicaldata/queue/ProcessQueue.java
@@ -0,0 +1,70 @@
+package co.edu.itm.clinicaldata.queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ProcessQueue {
+
+    private static ProcessQueue instance;
+    private Queue<String> queve;
+
+    private ProcessQueue() {
+        this.queve = new LinkedList<>();
+    }
+
+    /**
+     * Agrega a la cola de espera
+     * 
+     * @param process
+     */
+    public void add(String process) {
+        getInstance().queve.add(process);
+    }
+
+    /**
+     * Retorna y no remueve de la cola de espera
+     * 
+     * @return
+     */
+    public String get() {
+        return getInstance().queve.peek();
+    }
+
+    /**
+     * Retorna y remueve de la cola de espera
+     * 
+     * @return
+     */
+    public String remove() {
+        return getInstance().queve.poll();
+    }
+
+    /**
+     * Retorna instancia actual de la cola o crea una nueva de ser necesaria
+     * 
+     * @return
+     */
+    public static ProcessQueue getInstance() {
+        if (instance == null) {
+            instance = new ProcessQueue();
+        }
+        return instance;
+    }
+
+    /**
+     * Retorna el tamaño de la cola de espera
+     * 
+     * @return
+     */
+    public int size() {
+        return getInstance().queve.size();
+    }
+
+    /**
+     * Determina si la cola esta vacía
+     * @return
+     */
+    public boolean isEmpty() {
+        return getInstance().queve.isEmpty();
+    }
+}
