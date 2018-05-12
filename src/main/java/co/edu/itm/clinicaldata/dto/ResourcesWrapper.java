7c51d703d73397cd102cb61751aa2880ef17f044
Juan Camilo Velasquez <juancamilo---142@hotmail.com>
2018-05-11 22:05:55 -0500

    Se crea clase para manejar la lista de recursos requeridos.

---
+++ src/main/java/co/edu/itm/clinicaldata/dto/ResourcesWrapper.java
@@ -0,0 +1,17 @@
+package co.edu.itm.clinicaldata.dto;
+
+import java.util.List;
+
+public class ResourcesWrapper {
+
+    List<String> resources;
+
+    public List<String> getResources() {
+        return resources;
+    }
+
+    public void setResources(List<String> resources) {
+        this.resources = resources;
+    }
+
+}
