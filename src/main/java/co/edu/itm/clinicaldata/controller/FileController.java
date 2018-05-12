a2994e379dbc86de827756da7b42e5b0595b50af
Juan Camilo Velasquez <juancamilo---142@hotmail.com>
2018-05-11 22:05:30 -0500

    Se modifica servicio que sube archivo adicionando parámetro para recibir lista de recursos requeridos.

--- src/main/java/co/edu/itm/clinicaldata/controller/FileController.java
+++ src/main/java/co/edu/itm/clinicaldata/controller/FileController.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import co.edu.itm.clinicaldata.dto.ResourcesWrapper;
 import co.edu.itm.clinicaldata.exception.ValidateException;
 import co.edu.itm.clinicaldata.service.FileService;
 
@@ -22,10 +24,11 @@ public class FileController {
     @RequestMapping(value = "/upload", method = RequestMethod.POST)
     public ResponseEntity<String> upload(
             @RequestParam("file") MultipartFile file,
-            @RequestParam("investigatorId") Long investigatorId)
+            @RequestParam("investigatorId") Long investigatorId,
+            @ModelAttribute("resources") ResourcesWrapper resources)
             throws ValidateException {
         return new ResponseEntity<String>(fileService.upload(file,
-                investigatorId), HttpStatus.OK);
+                investigatorId, resources), HttpStatus.OK);
     }
 
 }
