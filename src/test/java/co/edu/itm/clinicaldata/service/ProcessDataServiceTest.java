28ad065519ff08f887a7cb562a0a12a583bbd564
Juan Camilo Velasquez <juancamilo---142@hotmail.com>
2018-06-11 18:21:57 -0500

    Pruebas para procesamiento de archivos.

---
+++ src/test/java/co/edu/itm/clinicaldata/service/ProcessDataServiceTest.java
@@ -0,0 +1,136 @@
+package co.edu.itm.clinicaldata.service;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import co.edu.itm.clinicaldata.component.FileUtilities;
+import co.edu.itm.clinicaldata.dto.Params;
+import co.edu.itm.clinicaldata.enums.ProcessState;
+import co.edu.itm.clinicaldata.exception.ValidateException;
+import co.edu.itm.clinicaldata.model.Investigator;
+import co.edu.itm.clinicaldata.model.ProcessingRequest;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ProcessDataServiceTest {
+
+    @Mock
+    InvestigatorService investigatorService;
+
+    @Mock
+    ProcessingRequestService processingRequestService;
+
+    @Mock
+    ClusterService clusterService;
+
+    @Mock
+    ProcessResourceService processResourceService;
+
+    @Mock
+    FileUtilities fileUtilities;
+
+    @InjectMocks
+    ProcessDataService processDataService;
+
+    @Test
+    public void processStateTest() throws ValidateException {
+        // arrange
+        String processIdentifier = "";
+        ProcessingRequest processingRequest = new ProcessingRequest();
+        processingRequest.setState(ProcessState.FINISHED_WITHOUT_ACTIONS.getState());
+        processingRequest.setCreationDate(new Timestamp(0));
+        Mockito.when(processingRequestService.validateAndFindByIdentifier(Mockito.anyString())).thenReturn(processingRequest);
+
+        // act
+        String message = processDataService.processState(processIdentifier);
+
+        // assert
+        Assert.assertNotNull(message);
+    }
+
+    @Test
+    public void processResultTest() throws ValidateException {
+        // arrange
+        String processIdentifier = "";
+        ProcessingRequest processingRequest = new ProcessingRequest();
+        processingRequest.setBasePath("basepath");
+        processingRequest.setState(ProcessState.FINISHED_OK.getState());
+        processingRequest.setCreationDate(new Timestamp(0));
+        Mockito.when(processingRequestService.validateAndFindByIdentifier(Mockito.anyString())).thenReturn(processingRequest);
+        Mockito.when(fileUtilities.readFile(Mockito.anyString())).thenReturn("Contenido leído");
+
+        // act
+        String message = processDataService.processResult(processIdentifier);
+
+        // assert
+        Assert.assertNotNull(message);
+    }
+
+    @Test(expected = ValidateException.class)
+    public void processResultCreatedTest() throws ValidateException {
+        // arrange
+        String processIdentifier = "";
+        ProcessingRequest processingRequest = new ProcessingRequest();
+        processingRequest.setState(ProcessState.CREATED.getState());
+        processingRequest.setCreationDate(new Timestamp(0));
+        Mockito.when(processingRequestService.validateAndFindByIdentifier(Mockito.anyString())).thenReturn(processingRequest);
+
+        // act
+        processDataService.processResult(processIdentifier);
+    }
+
+    @Test(expected = ValidateException.class)
+    public void processResultProcessingTest() throws ValidateException {
+        // arrange
+        String processIdentifier = "";
+        ProcessingRequest processingRequest = new ProcessingRequest();
+        processingRequest.setState(ProcessState.PROCESSING.getState());
+        processingRequest.setCreationDate(new Timestamp(0));
+        Mockito.when(processingRequestService.validateAndFindByIdentifier(Mockito.anyString())).thenReturn(processingRequest);
+
+        // act
+        processDataService.processResult(processIdentifier);
+    }
+
+    @Test
+    public void startProcessTest() throws ValidateException {
+        // arrange
+        Params params = new Params();
+        ProcessingRequest processingRequest = new ProcessingRequest();
+        processingRequest.setIdentifier("1233");
+        processingRequest.setState(ProcessState.CREATED.getState());
+        Mockito.when(processingRequestService.validateAndFindByIdentifier(Mockito.anyString())).thenReturn(processingRequest);
+        Investigator investigator = new Investigator();
+        investigator.setName("Juan");
+        Mockito.when(investigatorService.validateAndFind(Mockito.anyLong())).thenReturn(investigator);
+        Mockito.when(processResourceService.validateRequiredResources(Mockito.any(), Mockito.any(ProcessingRequest.class))).thenReturn(new ArrayList<>());
+        Mockito.when(processingRequestService.updateState(Mockito.any(), Mockito.any())).thenReturn(processingRequest);
+
+        // act
+        String message = processDataService.startProcess(params);
+
+        // assert
+        Assert.assertNotNull(message);
+    }
+
+    @Test(expected=ValidateException.class)
+    public void startProcessStateNotValidTest() throws ValidateException {
+        // arrange
+        Params params = new Params();
+        ProcessingRequest processingRequest = new ProcessingRequest();
+        processingRequest.setIdentifier("1233");
+        processingRequest.setState(ProcessState.FINISHED_OK.getState());
+        Mockito.when(processingRequestService.validateAndFindByIdentifier(Mockito.anyString())).thenReturn(processingRequest);
+
+        // act
+        processDataService.startProcess(params);
+    }
+
+}
