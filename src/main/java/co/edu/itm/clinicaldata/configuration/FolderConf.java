e3940d680ee58492ddf19b76f51bd04f8a8f0505
Juan Camilo Velasquez <juancamilo---142@hotmail.com>
2018-06-12 21:56:14 -0500

    Ajuste de get y set para atributos de configuración.

--- src/main/java/co/edu/itm/clinicaldata/configuration/FolderConf.java
+++ src/main/java/co/edu/itm/clinicaldata/configuration/FolderConf.java
@@ -20,32 +20,64 @@ public class FolderConf {
         return defaultFolder;
     }
 
-    public String getTemplateName() {
-        return templateName;
-    }
-
-    public String getErrOutputFile() {
-        return errOutputFile;
-    }
-
-    public String getLogOutputFile() {
-        return logOutputFile;
-    }
-
-    public String getShFileName() {
-        return shFileName;
-    }
-
-    public String getKeyToReplace() {
-        return keyToReplace;
+    public void setDefaultFolder(String defaultFolder) {
+        this.defaultFolder = defaultFolder;
     }
 
     public String getTemplateFolder() {
         return templateFolder;
     }
 
+    public void setTemplateFolder(String templateFolder) {
+        this.templateFolder = templateFolder;
+    }
+
     public String getResourcesFolder() {
         return resourcesFolder;
+    }
+
+    public void setResourcesFolder(String resourcesFolder) {
+        this.resourcesFolder = resourcesFolder;
+    }
+
+    public String getTemplateName() {
+        return templateName;
+    }
+
+    public void setTemplateName(String templateName) {
+        this.templateName = templateName;
+    }
+
+    public String getErrOutputFile() {
+        return errOutputFile;
+    }
+
+    public void setErrOutputFile(String errOutputFile) {
+        this.errOutputFile = errOutputFile;
+    }
+
+    public String getLogOutputFile() {
+        return logOutputFile;
+    }
+
+    public void setLogOutputFile(String logOutputFile) {
+        this.logOutputFile = logOutputFile;
+    }
+
+    public String getShFileName() {
+        return shFileName;
+    }
+
+    public void setShFileName(String shFileName) {
+        this.shFileName = shFileName;
+    }
+
+    public String getKeyToReplace() {
+        return keyToReplace;
+    }
+
+    public void setKeyToReplace(String keyToReplace) {
+        this.keyToReplace = keyToReplace;
     }
 
 }
